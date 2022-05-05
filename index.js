5998a78f5fa89b16acc44ed22c6d7ff71f5aec4a
shankuo <shankuo@gmail.com>
2022-05-05 19:08:16 +0800

    space add for Previous

--- index.js
+++ index.js
@@ -5,16 +5,16 @@
 let count = 0
 let countEl = document.getElementById('count-el');
 let saveEl = document.getElementById('save-el');
+console.log(saveEl);
 
 function increment() {
     count += 1;
-    console.log(count);
     countEl.textContent = count;
 }
 
 function save() {
     let countStr = count + " - ";
-    saveEl.textContent = countStr;
+    saveEl.textContent += countStr;
     count = 0;
     countEl.textContent = 0;
 }
